4fe8d8c78a46eb82e3502dc01c7e3a232b280f07
Kunal Rajan Deshmukh <edeskun@MacBook-Pro.local>
2018-09-18 12:32:59 -0700

    added a function to use tf dataset under utils

--- utils.py
+++ utils.py
@@ -9,3 +9,13 @@ def read_all_images(path,x,y):
         resized_image = tf.image.resize_images(image, [x, y])
         data.append(resized_image)
     return tf.stack(data)
+
+
+def train_input_fn(features, batch_size):
+    """ Input function for training """
+    #convert input to dataset
+    dataset = tf.data.Dataset.from_tensor_slices(features)
+    # Shuffle, repeat, and batch the examples.
+    dataset = dataset.shuffle(1000).repeat().batch(batch_size)
+    # Return the read end of the pipeline.
+    return dataset.make_one_shot_iterator().get_next()
